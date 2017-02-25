301540a62450893c9f6b818592c52a24b5abd51e
FujiHaruka <fujiharuka@realglobe.jp>
2017-02-25 12:54:02 +0900

    docker redisに接続できなかった

--- env/index.js
+++ env/index.js
@@ -4,12 +4,12 @@ let test = process.env.NODE_ENV === 'test'
 
 const port = {
   APP: env('APP_PORT', 3000),
-  REDIS: env('REDIS_PORT', 6370),
+  REDIS: env('REDIS_PORT', 6379),
   MONGO: env('MONGO_PORT', 27011)
 }
 
 const url = {
-  REDIS: env('REDIS_URL', `redis://localhost:${port.REDIS}`),
+  REDIS: env('REDIS_URL', `redis://localhost:${port.REDIS}/12`),
   MONGO: env('MONGO_URL', `mongodb://localhost:${port.MONGO}/${test ? 'eta-test' : 'eta'}`)
 }
 
--- server/app.js
+++ server/app.js
@@ -2,16 +2,26 @@ const Hub = require('sugo-hub')
 const Actor = require('sugo-actor')
 const { port } = require('../env')
 const { join } = require('path')
-const { actorModule } = require('./actor_module')
+const actorModule = require('./actor_module')
 const endpoints = require('./endpoints')
+const mongoose = require('mongoose')
+const { url } = require('../env')
 
 let server = Hub({
   static: [ join(__dirname, '../public') ],
   endpoints,
   localActors: {
     'actor': Actor(actorModule)
+  },
+  storage: {
+    redis: {
+      url: url.REDIS
+    }
   }
 })
+
+mongoose.set('debug', true)
+mongoose.connect(url.MONGO)
 
 console.log(`Server listening on port ${port.APP}`)
 server.listen(port.APP)
