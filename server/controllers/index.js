198cef3ceb9e9985628b8fd9f24dbab1f9883e71
FujiHaruka <fujiharuka@realglobe.jp>
2017-02-27 23:20:28 +0900

    Add setting controller file

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,12 @@
 const token = require('./token')
 const doc = require('./doc')
 const sentence = require('./sentence')
+const setting = require('./setting')
 const { withTokenAuth } = require('../helpers/actor_module_mw')
 
 module.exports = Object.assign(
   token,
   withTokenAuth(doc),
-  withTokenAuth(sentence)
+  withTokenAuth(sentence),
+  withTokenAuth(setting)
 )
