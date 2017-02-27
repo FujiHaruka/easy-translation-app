1dee485b902b3890e2dd25f34fc737c48e9d66b1
FujiHaruka <fujiharuka@realglobe.jp>
2017-02-27 23:19:19 +0900

    Connect sentence controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 const token = require('./token')
 const doc = require('./doc')
+const sentence = require('./sentence')
 const { withTokenAuth } = require('../helpers/actor_module_mw')
 
 module.exports = Object.assign(
   token,
-  withTokenAuth(doc)
+  withTokenAuth(doc),
+  withTokenAuth(sentence)
 )
