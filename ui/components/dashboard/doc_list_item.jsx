9e211ba36607c22e107ebb75444b78caca738b03
FujiHaruka <fujiharuka@realglobe.jp>
2017-03-08 22:12:59 +0900

    Enable to delete doc

--- ui/components/dashboard/doc_list_item.jsx
+++ ui/components/dashboard/doc_list_item.jsx
@@ -4,16 +4,45 @@ import IconButton from 'material-ui/IconButton'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
+import FlatButton from 'material-ui/FlatButton'
 import Paper from 'material-ui/Paper'
+import Dialog from 'material-ui/Dialog'
 import { grey400 } from 'material-ui/styles/colors'
 import moment from 'moment'
 import { pathTo } from '../../helpers/util'
-
-const paperStyle = {
-  margin: '1em'
-}
+import styles from '../../css/doc_list_item.css'
+import url from '../../helpers/url'
+import co from 'co'
+import { getState, Actions } from 'jumpstate'
 
 class DocListItem extends React.Component {
+  constructor () {
+    super()
+    const s = this
+    s.changeName = s.changeName.bind(s)
+    s.deleteFile = s.deleteFile.bind(s)
+    s.openChangeNameDialog = s.openChangeNameDialog.bind(s)
+    s.openDeleteDialog = s.openDeleteDialog.bind(s)
+    s.closeChangeNameDialog = s.closeChangeNameDialog.bind(s)
+    s.closeDeleteDialog = s.closeDeleteDialog.bind(s)
+    s.deleteActions = [
+      <FlatButton
+        label='Cancel'
+        onTouchTap={s.closeDeleteDialog}
+      />,
+      <FlatButton
+        label='Delete'
+        primary
+        onTouchTap={s.deleteFile}
+      />
+    ]
+    s.changeNameActions = []
+    s.state = {
+      isOpenDeleteDialog: false,
+      isOpenChangeNameDialog: false
+    }
+  }
+
   render () {
     const s = this
     let {
@@ -21,17 +50,31 @@ class DocListItem extends React.Component {
       did,
       updatedAt
     } = s.props
+    let {
+      isOpenDeleteDialog,
+      isOpenChangeNameDialog
+    } = s.state
     let update = moment(updatedAt).format('YYYY-MM-DD HH:mm')
     let secondaryText = `Last update: ${update}`
     return (
-      <Paper zDepth={1} style={paperStyle}>
+      <Paper zDepth={1} className={styles.paper}>
         <ListItem
-          rightIconButton={s.menu()}
           primaryText={name}
           secondaryText={secondaryText}
           secondaryTextLines={1}
-          onClick={pathTo(`/dashboard/docs/${did}`)}
-      />
+          rightIconButton={s.menu()}
+          onTouchTap={pathTo(url.docPageOnListView(did))}
+          />
+
+        <Dialog
+          title='Confirmation'
+          actions={s.deleteActions}
+          modal={false}
+          open={isOpenDeleteDialog}
+          onRequestClose={s.closeDeleteDialog}
+          >
+          Are you sure to delete the file?
+        </Dialog>
       </Paper>
     )
   }
@@ -39,9 +82,10 @@ class DocListItem extends React.Component {
   menu () {
     const s = this
     return (
-      <IconMenu iconButtonElement={s.iconButtonElement()}>
-        <MenuItem>Change name</MenuItem>
-        <MenuItem>Delete</MenuItem>
+      <IconMenu
+        iconButtonElement={s.iconButtonElement()}>
+        <MenuItem onTouchTap={s.openChangeNameDialog}>Change file name</MenuItem>
+        <MenuItem onTouchTap={s.openDeleteDialog}>Delete file</MenuItem>
       </IconMenu>
     )
   }
@@ -56,6 +100,40 @@ class DocListItem extends React.Component {
         <MoreVertIcon color={grey400} />
       </IconButton>
     )
+  }
+
+  changeName () {
+    const s = this
+    s.closeChangeNameDialog()
+  }
+
+  deleteFile () {
+    const s = this
+    return co(function * () {
+      let state = getState()
+      let { api } = state.caller
+      let { userKey, token } = state.user
+      let id = s.props.did
+      yield api.deleteDoc({ userKey, token, id })
+      s.closeDeleteDialog()
+      yield Actions.fetchDocs()
+    })
+  }
+
+  openDeleteDialog () {
+    this.setState({ isOpenDeleteDialog: true })
+  }
+
+  openChangeNameDialog () {
+    this.setState({ isOpenChangeNameDialog: true })
+  }
+
+  closeDeleteDialog () {
+    this.setState({ isOpenDeleteDialog: false })
+  }
+
+  closeChangeNameDialog () {
+    this.setState({ isOpenChangeNameDialog: false })
   }
 }
 
